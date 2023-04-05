52af48d3b998774e7073a04e3d9cf6d2bad08264
Arief Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2023-04-06 05:30:05 +0700

    One Tap Sign in with google

--- app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/AuthenticationScreen.kt
@@ -4,20 +4,40 @@ import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.arifahmadalfian.diaryapp.util.Constants.CLIENT_ID
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
 
 @ExperimentalMaterial3Api
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
     loadingState: Boolean,
+    oneTapState: OneTapSignInState,
+    messageBarState: MessageBarState,
     onButtonClicked: () -> Unit
 ) {
     Scaffold(
         content = {
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
+        }
+    )
+
+    OneTapSignInWithGoogle(
+        state = oneTapState,
+        clientId = CLIENT_ID,
+        onTokenIdReceived = { tokenId ->
+            messageBarState.addSuccess("Token ID: $tokenId")
+        },
+        onDialogDismissed = { message ->
+            messageBarState.addError(Exception(message))
         }
     )
 }
--- app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/route/AuthenticationRoute.kt
+++ app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/route/AuthenticationRoute.kt
@@ -5,14 +5,21 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.arifahmadalfian.diaryapp.navigation.Screen
 import com.arifahmadalfian.diaryapp.presentation.screen.auth.AuthenticationScreen
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
 
 @OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
-        AuthenticationScreen(
-            loadingState = false,
-            onButtonClicked = {
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
 
+        AuthenticationScreen(
+            loadingState = oneTapState.opened,
+            oneTapState = oneTapState,
+            messageBarState = messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
             }
         )
     }
