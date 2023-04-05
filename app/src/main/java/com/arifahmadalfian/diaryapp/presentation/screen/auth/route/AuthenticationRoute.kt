861e2486ff33974e390ffa1c6a5101d1f6ae5b4f
Arief Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2023-04-06 06:19:01 +0700

    JWT Authentication Provider with Mongo DB

--- app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/route/AuthenticationRoute.kt
+++ app/src/main/java/com/arifahmadalfian/diaryapp/presentation/screen/auth/route/AuthenticationRoute.kt
@@ -1,5 +1,6 @@
 package com.arifahmadalfian.diaryapp.presentation.screen.auth.route
 
+import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
